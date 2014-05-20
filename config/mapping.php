dbe3b433ea1b8fccbc6b708ad35fe95920cac3b3
pengshen001 <peng.shen@indochino.com>
2014-05-19 21:20:03 -0700

    init

---
+++ config/mapping.php
@@ -0,0 +1,9 @@
+<?php
+register('/index', new ConfoneHomeController());
+
+register('/about/index',   new AboutIndexController());
+register('/about/contact', new AboutContactController());
+register('/about/privacy', new AboutPrivacyController());
+register('/about/team',    new AboutTeamController());
+register('/about/terms',   new AboutTermsController());
+?>
